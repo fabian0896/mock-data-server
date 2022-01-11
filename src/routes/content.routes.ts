66d167495d1ebab26556010775a459f82d9a86a0
Imac <imac@iMac-de-Imac.local>
2022-01-11 17:08:23 -0500

    [UPDATE] content service with prisma

--- src/routes/content.routes.ts
+++ src/routes/content.routes.ts
@@ -5,14 +5,13 @@ import { getContentSchema } from '../schemas/content.schema';
 import validationHandler from '../middleware/validation.handler';
 
 const router = Router();
-const contentService = new ContentService();
 
 router.get('/', 
   validationHandler(getContentSchema, 'query'),
   async (req, res, next) => {
     const { page } = req.query;
     try {
-      const content = contentService.getAllContent(Number(page ?? 1));
+      const content = await ContentService.getAllContent(Number(page ?? 1));
       res.json(content);
     } catch (error) {
       if (error instanceof Error) {
--- src/services/content.service.ts
+++ src/services/content.service.ts
@@ -1,31 +1,27 @@
-import  content from '../database.json';
-import { Content } from '../models/content';
-
 import prisma from '../lib/prisma';
 
 export default class ContentService {
-  pages: any[] = [];
-  chunks = ~~(content.length / 20);
-  constructor() {
-    for (let i = 0; i < this.chunks; i++) {
-      const start = i * 20;
-      const end = start + 20;
-      const page = content.slice(start, end);
-      this.pages.push(page);
-    }
-  }
-
-  getAllContent(page: number = 1) {
+  static async getAllContent(page: number = 1) {
     if (page < 1) throw new Error('page can not be lower than 1');
-      return {
-        page,
-        results: this.pages[page - 1],
-        totalPages: this.pages.length,
-        next: page + 1 > this.pages.length ? null : page + 1,
-      };
+    const results = await prisma.course.findMany({
+      include: {
+        teacher: {
+          include: {
+            country: true,
+            picture: true,
+          }
+        },
+        content: {
+          include: {
+            course: true,
+          }
+        },
+      },
+      take: 20,
+    });
+    return results;
   }
 }
-
 /* module.exports = function ContentService() {
     const chunks = ~~(content.length / 20);
     const pages = [];
@@ -51,7 +47,3 @@ export default class ContentService {
       getAllContent,
     }
 } */
-
-
-
-
